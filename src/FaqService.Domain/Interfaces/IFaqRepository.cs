230bbd24c7447c41f52f67e2eb9cdfe83a91af8a
Alnils <Albin.nilsson.se@gmail.com>
2026-05-13 17:06:13 +0200

    Created IFaqRepository Interface

---
+++ src/FaqService.Domain/Interfaces/IFaqRepository.cs
@@ -0,0 +1,18 @@
+﻿using FaqService.Domain.Entities;
+
+namespace FaqService.Domain.Interfaces;
+
+public interface IFaqRepository
+{
+    Task<IEnumerable<Faq>> GetAllAsync();
+
+    Task<Faq?> GetByIdAsync(int id);
+
+    Task<Faq?> CreateAsync(Faq faq);
+
+    Task<Faq?> UpdateAsync(int id, Faq faq);
+
+    Task<bool> DeleteAsync(int id);
+
+    Task<int> GetMaxDisplayOrderAsync();
+}
