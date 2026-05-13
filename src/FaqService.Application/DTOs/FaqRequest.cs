97249737a29e984c1330253d2f02ff385dc877e8
Alnils <Albin.nilsson.se@gmail.com>
2026-05-13 22:10:03 +0200

    Added dataannotations for requests

--- src/FaqService.Application/DTOs/FaqRequest.cs
+++ src/FaqService.Application/DTOs/FaqRequest.cs
@@ -1,8 +1,18 @@
-﻿namespace FaqService.Application.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaqService.Application.DTOs;
 
 public record FaqRequest
     (
+    [Required(ErrorMessage = "Title is required")]
+    [MaxLength(50, ErrorMessage = "Title cannot exceed 50 characters")]
     string Title,
+
+    [Required(ErrorMessage = "Summary is required")]
+    [MaxLength(500, ErrorMessage = "Summary cannot exceed 500 characters")]
     string Summary,
+
+    [Required(ErrorMessage = "Content is required")]
+    [MaxLength(5000, ErrorMessage = "Content cannot exceed 5000 characters")]
     string Content
     );
